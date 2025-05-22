7f53a7e053f2657f7ae3cb372cadd89c9395534c
HeMuellerBKM <212855705+HeMuellerBKM@users.noreply.github.com>
2025-05-22 13:43:01 +0200

    Update antwort.py

--- antwort.py
+++ antwort.py
@@ -1,5 +1,20 @@
 def is_correct(question, answer_index): 
   return question ["correct"] == answer_index
 
+if __name__ == "__main__":
+    questions = {
+      "text": "Was ist das Chemische Zeichen von Wasser"
+      "option": ["H2O","NaOH","CO2","O2"]
+      "correct": 1
+    }
+      
 
-questions 
+    answer = 1
+    print("Question:", question["text"])
+    for i, option in enumerate(question["options"]):
+        print(f"{i + 1}. {option}")
+
+    if is_correct(question, answer):
+        print("Richtig")
+    else:
+        print("Falsch")
